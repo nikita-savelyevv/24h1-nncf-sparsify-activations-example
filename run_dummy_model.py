13beabb746010674e05824fd26afd28ead920c23
Nikita Savelyev <nikita.savelyev@intel.com>
2024-08-13 15:37:41 +0200

    Adopt run_dummy_model.py for OV backend

--- run_dummy_model.py
+++ run_dummy_model.py
@@ -11,7 +11,7 @@ import openvino as ov
 import nncf
 import nncf.experimental
 import nncf.experimental.torch.sparsify_activations
-from nncf.scopes import IgnoredScope
+from nncf.experimental.torch.sparsify_activations import TargetScope
 
 
 class ThreeLinearModel(nn.Module):
@@ -62,7 +62,7 @@ class ModelDesc:
     name: str
     model_getter: Callable[[], nn.Module]
     dataset_getter: Callable[[torch.device], nncf.Dataset]
-    target_sparsity_by_scope: Dict[str, float]
+    target_sparsity_by_scope: Dict[TargetScope, float]
     ignored_scope: Optional[nncf.IgnoredScope]
 
 
@@ -72,7 +72,7 @@ model_list = [
         model_getter=lambda: nn.Linear(8, 16),
         dataset_getter=lambda: nncf.Dataset(torch.randn([3, 2, 8])),
         target_sparsity_by_scope={
-            "{re}.*linear.*": 0.3,
+            TargetScope(patterns=[".*linear.*"]): 0.3,
         },
         ignored_scope=None,
     ),
@@ -81,7 +81,7 @@ model_list = [
         model_getter=MatMulModel,
         dataset_getter=lambda: nncf.Dataset(torch.randn([3, 2, 8])),
         target_sparsity_by_scope={
-            "{re}.*linear.*": 0.3,
+            TargetScope(patterns=[".*linear.*"]): 0.3,
         },
         ignored_scope=None,
     ),
@@ -90,7 +90,7 @@ model_list = [
         model_getter=ThreeLinearModel,
         dataset_getter=lambda: nncf.Dataset(torch.randint(0, 30, (3, 2, 8))),
         target_sparsity_by_scope={
-            "{re}.*linear.*": 0.4,
+            TargetScope(patterns=[".*linear.*"]): 0.4,
         },
         ignored_scope=None,
     ),
@@ -99,41 +99,53 @@ model_list = [
         model_getter=dummy_llama_model,
         dataset_getter=lambda: nncf.Dataset(torch.randint(0, 30, (3, 2, 8))),
         target_sparsity_by_scope={
-            "{re}.*gate_proj.*": 0.2,
-            "{re}.*up_proj.*": 0.3,
-            "{re}.*down_proj.*": 0.4,
+            TargetScope(patterns=[".*gate_proj.*"]): 0.2,
+            TargetScope(patterns=[".*up_proj.*"]): 0.3,
+            TargetScope(patterns=[".*down_proj.*"]): 0.4,
         },
         ignored_scope=None,
     ),
 ]
 
 
-def export_sparse_ir(desc: ModelDesc, compress_weights: bool):
-    print(desc, 'compress_weights=', compress_weights)
+def export_sparse_ir(desc: ModelDesc, ov_backend: bool, compression_bits: Optional[int]):
+    assert compression_bits in [None, 4, 8]
+    assert ov_backend or compression_bits != 4
     model = desc.model_getter().eval()
     dataset = desc.dataset_getter()
-    if compress_weights:
-        model = nncf.compress_weights(
-            model,
-            mode=nncf.CompressWeightsMode.INT8_ASYM,
-            dataset=dataset,
-        )
-    # model = nncf.experimental.torch.sparsify_activations.sparsify_activations(
-    #     model=model,
-    #     dataset=dataset,
-    #     target_sparsity_by_scope=desc.target_sparsity_by_scope,
-    #     ignored_scope=desc.ignored_scope,
-    # )
     example_input = next(iter(dataset.get_inference_data()))
-    ov_model = ov.convert_model(model, example_input=example_input)
-    compiled_model = ov.compile_model(ov_model, "CPU", config={ov.properties.hint.inference_precision: "f32"})
+    if ov_backend:
+        model = ov.convert_model(model, example_input=example_input)
+    if compression_bits is not None:
+        kwargs = {
+            "mode": nncf.CompressWeightsMode.INT8_ASYM if compression_bits == 8 else nncf.CompressWeightsMode.INT4_ASYM,
+            "ignored_scope": desc.ignored_scope
+        }
+        if not ov_backend:
+            kwargs["dataset"] = dataset
+        if compression_bits == 4:
+            kwargs["group_size"] = 1
+        model = nncf.compress_weights(model, **kwargs)
+    model = nncf.experimental.torch.sparsify_activations.sparsify_activations(
+        model=model,
+        dataset=dataset,
+        target_sparsity_by_scope=desc.target_sparsity_by_scope,
+        ignored_scope=desc.ignored_scope,
+    )
+    if not ov_backend:
+        model = ov.convert_model(model, example_input=example_input)
+    compiled_model = ov.compile_model(model, "CPU", config={ov.properties.hint.inference_precision: "f32"})
     compiled_model(example_input.cpu())
 
-    model_path = f'./dummy_models/{desc.name}_int8_sparse.xml' if compress_weights else f'./dummy_models/{desc.name}_sparse.xml'
-    ov.save_model(ov_model, model_path, compress_to_fp16=False)
+    save_dir = Path(f"./dummy_models/{'ov' if ov_backend else 'pt'}")
+    save_filename = f"{desc.name}{'_int8' if compression_bits == 8 else '_int4' if compression_bits == 4 else ''}_sparse.xml"
+    ov.save_model(model, save_dir / save_filename, compress_to_fp16=False)
 
 
 if __name__ == '__main__':
-    for compress_weights in [False, True]:
-        for desc in model_list:
-            export_sparse_ir(desc, compress_weights)
+    for desc in model_list:
+        for ov_backend in [True, False]:
+            for compression_bits in [None, 4, 8]:
+                if not ov_backend and compression_bits == 4:
+                    continue
+                export_sparse_ir(desc, ov_backend, compression_bits)
